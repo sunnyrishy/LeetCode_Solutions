7bbba252612fb1bb10c7f43c63b0183cb650578d
Sunny Rishy Vardhan <sunnyrishyvardhan@gmail.com>
2026-01-15 23:11:40 +0530

    Add 1_two_sum.cpp - LeetCode Problem 1

---
+++ 1_two_sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int>mpp;
+        int n = nums.size();
+        vector<int> ans;
+        for(int i = 0; i < n; i++){
+            int val1 = nums[i];
+            int val2 = target - val1;
+            if(mpp.find(val2) != mpp.end()){
+                ans.push_back(mpp[val2]);
+                ans.push_back(i);
+            }
+            mpp[val1] = i;
+        }
+        return ans;
+        
+    }
+};

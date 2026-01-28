9d9403460404f28aedfd131f2a9b5c88bd0d0c79
Sunny Rishy Vardhan <sunnyrishyvardhan@gmail.com>
2026-01-27 20:56:38 -0500

    Time: 0 ms (100%), Space: 23.9 MB (19.34%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -4,14 +4,14 @@ public:
         int left = -1;
         int right = -1;
         for(int i = 0; i < nums.size(); i++){
-            if(nums[i] == 0){
+            if(nums[i] == 0){  //first occurance of zero
                 left = i;
                 break;
             }
         }
 
         for(int i = left; i < nums.size(); i++){
-            if(nums[i] != 0){
+            if(nums[i] != 0){  //first occurance of non zero after the first zero
                 right = i;
                 break;
             }

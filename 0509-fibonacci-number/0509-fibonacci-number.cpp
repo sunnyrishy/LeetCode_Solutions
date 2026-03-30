01bf301071cdfc6c0f12cad2aa6186cc31375330
Sunny Rishy Vardhan <sunnyrishyvardhan@gmail.com>
2026-03-29 23:16:58 -0400

    Time: 0 ms (100%), Space: 7.7 MB (87.64%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-//using tabulation
     int fib(int n) {
         if(n <= 1) return n;
-        vector<int> dp(n+1, -1); //step 0
-        //base case  //step 1
-        dp[0] = 0;
-        dp[1] = 1;
+        int prev2 = 0;
+        int prev = 1;
         for(int i = 2; i <= n; i++){
-            dp[i] = dp[i-1] + dp[i-2];  //step 2
+            int curr = prev + prev2;
+            prev2 = prev;
+            prev = curr;
         }
-        return dp[n];
+        return prev;
+        
     }
 };

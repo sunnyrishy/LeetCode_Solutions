ae3adebff364e6bc6ed6f0d2edc0531455b06f76
Sunny Rishy Vardhan <sunnyrishyvardhan@gmail.com>
2026-03-29 23:15:34 -0400

    Time: 3 ms (65.32%), Space: 8.1 MB (15.75%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
-// using memoization (top-bottom approach)
-//we solve the sub problem and store its result
-
-    int f(int n, vector<int>&dp){
-        if(n <= 1) return n;
-        if(dp[n] != -1) return dp[n]; //step 1
-        dp[n] = f(n-1, dp) + f(n-2, dp); //step 2
-        return dp[n];  
-    }
-
+//using tabulation
     int fib(int n) {
+        if(n <= 1) return n;
         vector<int> dp(n+1, -1); //step 0
-        return f(n, dp);
+        //base case  //step 1
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];  //step 2
+        }
+        return dp[n];
     }
 };

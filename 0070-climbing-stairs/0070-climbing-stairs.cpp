e1a38637e4c450441d0a5fc14712812f48e4976f
Sunny Rishy Vardhan <sunnyrishyvardhan@gmail.com>
2026-03-29 23:50:46 -0400

    Time: 0 ms (100%), Space: 7.9 MB (69.02%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n == 0) return 1;
+        if(n == 1) return 1;
+        int prev2 = 1;
+        int prev = 2;
+        for(int i = 3; i <= n; i++){
+            int curr = prev + prev2;
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
+    }
+};

db36f2bcc50381c3062217ebb34597d20788ccb4
Sunny Rishy Vardhan <sunnyrishyvardhan@gmail.com>
2026-04-06 23:40:49 -0400

    Time: 0 ms (100%), Space: 15.2 MB (67.12%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        //we have atleast one portion to be sorted either left or right
-        //sorted we have two options 1. it is useful 2. it is not useful
-        //unsorted we have two options 1. it is useful 2. it is not useful
-        int n = nums.size();
+        //find the sorted protion and check if that portion is useful to us or not.
         int low = 0;
-        int high = n-1;
+        int high = nums.size()-1;
         while(low <= high){
             int mid = low + (high-low)/2;
             if(nums[mid] == target) return mid;
-            if(nums[low] <= nums[mid]){  //left portion is sorted
-                if(target >= nums[low] && target <= nums[mid]){ //useful
-                    high = mid -1;
+            //if left portion is sorted ?
+            if(nums[low] <= nums[mid]){
+                if(target >= nums[low] && target <= nums[mid]){
+                    high = mid -1; //useful
+                } //not useful
+                else low = mid + 1;
+            }
+            else{ // this considers the right portion to be sorted.
+                if(target >= nums[mid] && target <= nums[high]){
+                    low = mid + 1;  //useful
                 }
                 else{  //not useful
-                    low = mid + 1;
-                }
-            }
-            else{  //rightportion is not sorted
-                if(target >= nums[mid] && target <= nums[high]){
-                    low = mid + 1;
-                }
-                else{
-                    high = mid -1;
+                    high = mid - 1;
                 }
             }
         }

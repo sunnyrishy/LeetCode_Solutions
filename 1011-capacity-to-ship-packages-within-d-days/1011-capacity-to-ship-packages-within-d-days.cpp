3ef8d865530e3d6e0074e66427d700878ef15fb7
Sunny Rishy Vardhan <sunnyrishyvardhan@gmail.com>
2026-04-09 20:33:16 -0400

    Time: 11 ms (67.08%), Space: 35 MB (53.34%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,36 +1,42 @@
 class Solution {
 public:
-    int cal_days(vector<int>&weights, int cap){
-        int days = 1;
-        int total_weight = 0;
+    int cal_days(vector<int>& weights, int days, int capacity){
+        int total_days = 1;
+        int curr_capacity = 0;
         for(int i = 0; i < weights.size(); i++){
-            if(total_weight + weights[i] > cap){
-                days++;
-                total_weight = weights[i];
+            if(curr_capacity + weights[i] <= capacity){
+                curr_capacity += weights[i];
             }
             else{
-                total_weight += weights[i];
+                total_days++;
+                curr_capacity = 0;
+                curr_capacity = weights[i];
             }
+            if(total_days > days) return total_days;
         }
-        return days;
+        return total_days;
     }
 
     int shipWithinDays(vector<int>& weights, int days) {
         int n = weights.size();
         int low = *max_element(weights.begin(), weights.end());
-        int high = accumulate(weights.begin(), weights.end(), 0);
+        int high = 0;
+        for(int i = 0; i < n; i++){
+            high += weights[i];
+        }
         int ans = high;
         while(low <= high){
             int mid = low + (high-low)/2;
-            int days_req = cal_days(weights, mid);
-            if(days_req > days){
+            int no_days = cal_days(weights, days, mid);
+            if(no_days > days){
                 low = mid + 1;
             }
             else{
                 ans = mid;
-                high = mid - 1;
+                high = mid -1;
             }
         }
         return ans;
+        
     }
 };

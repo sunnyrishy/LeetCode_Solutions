c272c8ea65284d6cd43ee0bd92e6e19e70ac4eed
Sunny Rishy Vardhan <sunnyrishyvardhan@gmail.com>
2026-05-16 23:20:03 -0400

    Time: 23 ms (8.25%), Space: 39.3 MB (36.16%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool canReach(vector<int>& arr, int start) {
+        if(arr[start] == 0) return true;
+        int n = arr.size();
+        queue<int> q;
+        set<int> st;
+        q.push(start);
+        while(!q.empty()){
+            int curr = arr[q.front()];
+            if(curr == 0) return true;
+            int right = q.front() + curr;
+            int left = q.front() - curr;
+            if(right <= n-1 && st.find(right) == st.end()) q.push(right);
+            if(left >= 0 && st.find(left) == st.end()) q.push(left);
+            st.insert(q.front());
+             q.pop();
+        }
+        return false;
+    }
+};

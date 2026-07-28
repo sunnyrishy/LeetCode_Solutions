140d7b22e04e1ed950149d5f4f489618420b2724
Sunny Rishy Vardhan <sunnyrishyvardhan@gmail.com>
2026-07-28 15:37:21 -0400

    Time: 27 ms (85.21%), Space: 82.7 MB (87.32%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        map<int, int> mpp;
-        for(int i = 0; i < nums.size(); i++){
-            mpp[nums[i]]++;
-            if(mpp[nums[i]] > 1) return true;
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        for(int i = 1; i < n; i++){
+            if(nums[i] == nums[i-1]) return true;
         }
         return false;
     }

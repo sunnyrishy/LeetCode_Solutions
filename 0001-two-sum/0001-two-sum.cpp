e6bf622978db5efc1b05ae497d7ce607de81d9cb
Sunny Rishy Vardhan <sunnyrishyvardhan@gmail.com>
2026-07-28 16:30:00 -0400

    Time: 2 ms (72.12%), Space: 14.9 MB (39.55%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        // most optimal 
+        //we need the target-nums[i] value and its index so use unordered_map
         unordered_map<int, int> mpp;
-        for(int i = 0; i < nums.size(); i++){
-            if(mpp.find(target-nums[i]) != mpp.end()){
-                int idx = mpp[target-nums[i]];
-                return {i, idx};
+        for(int i = 0; i < nums.size(); i++){ //O(n)
+            auto it = mpp.find(target-nums[i]);
+            if(it != mpp.end()){
+                return {i, it->second};
             }
             mpp[nums[i]] = i;
 
         }
         return {-1,-1};
+        //T = O(n) and S = O(n)
         
     }
 };

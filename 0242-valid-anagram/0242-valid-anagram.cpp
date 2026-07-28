d6fc07ea3090f0d192365041010fc6cbc837e7f1
Sunny Rishy Vardhan <sunnyrishyvardhan@gmail.com>
2026-07-28 16:14:16 -0400

    Time: 0 ms (100%), Space: 9.8 MB (50.1%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        //most optimal
+        // we just need to check if the freq of char in both string are same or not.
         if(s.size() != t.size()) return false;
         int n = s.size();
         int m = t.size();
         vector<int> freq(26, 0);
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < n; i++){ //O(n)
             freq[s[i]-'a']++;
             freq[t[i]-'a']--;
         }
-        for(int i = 0; i < 26; i++){
+        for(int i = 0; i < 26; i++){ //O(26)
             if(freq[i] != 0) return false;
         }
         return true;
+        //T = O(n) and S = O(26)== O(1)
         
     }
 };

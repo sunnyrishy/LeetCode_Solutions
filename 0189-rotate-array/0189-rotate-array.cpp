30ad4674cc96090cc4427e822033defaea7a6644
Sunny Rishy Vardhan <sunnyrishyvardhan@gmail.com>
2026-08-24 15:48:51 -0400

    Time: 12 ms (5.33%), Space: 263.4 MB (7.65%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -3,8 +3,8 @@ public:
     void rotate(vector<int>& arr, int k) {
         int n = arr.size();
         k = k % n;
-        reverse(arr.begin()+n-k-1, arr.begin()+n);
-        reverse(arr.begin(), arr.begin()+n-k-1);
-        reverse(arr.begin(), arr.begin()+n-1);
+        reverse(arr.begin()+n-k-1, arr.begin()+n);  //second portion
+        reverse(arr.begin(), arr.begin()+n-k-1);  //first portion
+        reverse(arr.begin(), arr.begin()+n-1);  //whole portion
     }
 };

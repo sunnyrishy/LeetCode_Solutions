770788235abd35457e396ed4de004b84f1e12d94
Sunny Rishy Vardhan <sunnyrishyvardhan@gmail.com>
2026-08-25 11:23:59 -0400

    Time: 3 ms (52.62%), Space: 24 MB (39.52%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -5,24 +5,24 @@ public:
         //check cols
         //check 3x3 boxes
         for(int i = 0; i < 9; i++){ //rows
-            set<int> st;
+            unordered_set<int> st;
             for(int j = 0; j<9; j++){
                 if (board[i][j] == '.') continue;
                 if(st.count(board[i][j])) return false;
                 st.insert(board[i][j]);
             }
         }
-        for(int j = 0; j< 9; j++){
-            set<int> st;
+        for(int j = 0; j< 9; j++){ //cols
+            unordered_set<int> st;
             for(int i = 0; i < 9; i++){
                 if (board[i][j] == '.') continue;
                 if(st.count(board[i][j])) return false;
                 st.insert(board[i][j]);
             }
         }
-        for(int i = 0; i < 9; i+=3){
+        for(int i = 0; i < 9; i+=3){ //3x3 boxes
             for(int j = 0; j < 9; j+=3){
-                set<int> st;
+                unordered_set<int> st;
                 for(int k = i; k < i+3; k++){
                     for(int l = j; l < j+3; l++){
                         if (board[k][l] == '.') continue;

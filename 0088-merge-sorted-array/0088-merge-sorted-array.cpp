3e4f575f1ee2adfd6bcac2cb6d1bbdb2dcef8cf5
Sunny Rishy Vardhan <sunnyrishyvardhan@gmail.com>
2026-09-01 20:41:26 -0400

    Time: 0 ms (100%), Space: 12.3 MB (81.46%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,26 +1,35 @@
 class Solution {
 public:
+
+// doing this in reverse order
+//always deal with the current largest element and push it to the corner of nums1 vector.
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         int i = m-1;
         int j = n-1;
-        int k = nums1.size()-1;
+        int k = n+m-1;
         while(i>= 0 && j >= 0){
-            if(nums1[i] >= nums2[j]){
-                nums1[k] = nums1[i];
-                k--;
-                i--;
-            }
-            else{
+            if(nums2[j] > nums1[i]){
                 nums1[k] = nums2[j];
-                k--;
                 j--;
             }
-        }
-        while(j>=0){
-            nums1[k] = nums2[j];
-            j--;
+            else{
+                nums1[k] = nums1[i];
+                i--;
+            }
             k--;
         }
-
+        while(j>= 0){
+            nums1[k] = nums2[j];
+            k--;
+            j--;
+        }
+        return;
     }
 };
+
+// try some test cases like 
+// nums1 = [1,2,3,0,0,0], m = 3
+// nums2 = [4,5,6], n = 3
+
+// Expected:
+// [1,2,3,4,5,6]

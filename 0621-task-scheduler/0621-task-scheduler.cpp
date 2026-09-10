12ded7cd053a56bbf3f83f2224db1710d77152e1
Sunny Rishy Vardhan <sunnyrishyvardhan@gmail.com>
2026-09-09 20:26:58 -0400

    Time: 39 ms (30.32%), Space: 47.6 MB (38.18%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        map<char, int> mpp;
+        for(int i = 0; i < tasks.size(); i++){
+            mpp[tasks[i]]++;
+        }
+        priority_queue<int> max_heap;
+        for(auto it : mpp){
+            max_heap.push(it.second);
+        }
+        int time = 0;
+        while(!max_heap.empty()){
+            vector<int> temp;
+            for(int i = 0; i < n+1; i++){
+                if(!max_heap.empty()){
+                    temp.push_back(max_heap.top());
+                    max_heap.pop();
+                }
+            }
+            for(int num : temp){
+                if(--num > 0){
+                    max_heap.push(num);
+                }
+            }
+            time += max_heap.empty() ? temp.size() : n+1;
+        }
+        return time;
+        
+    }
+};

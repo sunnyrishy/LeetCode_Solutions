a97854c0c00b7add3594adf42b8a4c4947edb253
Sunny Rishy Vardhan <sunnyrishyvardhan@gmail.com>
2026-09-09 20:34:44 -0400

    Time: 43 ms (21.42%), Space: 47.8 MB (10.99%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -5,10 +5,12 @@ public:
         for(int i = 0; i < tasks.size(); i++){
             mpp[tasks[i]]++;
         }
+        //count freq
         priority_queue<int> max_heap;
         for(auto it : mpp){
             max_heap.push(it.second);
         }
+        //push the freq to max heap
         int time = 0;
         while(!max_heap.empty()){
             vector<int> temp;
@@ -17,6 +19,7 @@ public:
                     temp.push_back(max_heap.top());
                     max_heap.pop();
                 }
+                //this will be the cycle
             }
             for(int num : temp){
                 if(--num > 0){
@@ -24,6 +27,7 @@ public:
                 }
             }
             time += max_heap.empty() ? temp.size() : n+1;
+            //if the characters are repeating, it will take n+1 time, else single time (taken from temp arrya)
         }
         return time;
         
